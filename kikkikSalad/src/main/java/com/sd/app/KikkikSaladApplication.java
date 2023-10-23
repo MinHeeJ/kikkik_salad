4e4a6a66695acaff3ee37d70800c0c3870fdf815
MinheeJ <mhjeeeee15@gmail.com>
2023-10-23 15:52:12 +0900

    commit

--- kikkikSalad/src/main/java/com/sd/app/KikkikSaladApplication.java
+++ kikkikSalad/src/main/java/com/sd/app/KikkikSaladApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KikkikSaladApplication {
 
 	public static void main(String[] args) {
+		// 테스트용 주석
 		SpringApplication.run(KikkikSaladApplication.class, args);
 	}
 
